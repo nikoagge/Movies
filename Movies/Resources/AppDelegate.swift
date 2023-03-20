5b8a436ddbb3e16d0da98861ac40643d5365cd0a
nikoagge <n.aggelidis@m-stat.gr>
2023-03-20 09:25:52 +0200

    Removed SceneDelegate from project that caused missed Info.plist crash

--- Movies/Resources/AppDelegate.swift
+++ Movies/Resources/AppDelegate.swift
@@ -7,17 +7,17 @@
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let listOfShowsViewController = ListOfShowsViewController()
+        let navigationController = UINavigationController(rootViewController: listOfShowsViewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
         return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
 }
