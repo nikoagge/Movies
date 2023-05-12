43f3e4907e2daa84d7c788b3c5f8e8bf517a4d4b
nikoagge <nikoagge@gmail.com>
2023-05-12 13:08:37 +0300

    Basic mvvm implementation in order to fetch and display shows

--- Movies/ViewControllers/ListOfShows/ListOfShowsViewController.swift
+++ Movies/ViewControllers/ListOfShows/ListOfShowsViewController.swift
@@ -10,42 +10,104 @@ import UIKit
 final class ListOfShowsViewController:
     UIViewController,
     NavigableViewController {
+    let listOfShowsTableView = UITableView.newAutoLayout()
     let listOfShowsViewModel = ListOfShowsViewModel()
     
-    let listOfShowsTableView = UITableView.newAutoLayout()
-    
-    typealias CompletionClosure = (([ServiceShow]?, HTTPError?) -> Void)
-
-//    private var networkManager:NetworkManagerType = NetworkManager()
     private let realmManager: RealmManagerType = RealmManager()
-    
-    var listOfServiceShows = [ServiceShow]()
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupUI()
         
-        ShowsAPI.shared.getShows { result in
-            switch result {
-            case .success(let success):
-                debugPrint(success)
-            case .failure(let failure):
-                debugPrint(failure)
-
-            }
-        }
-        
-//        loadData()
+        loadData()
     }
     
     private func loadData() {
-//        listOfShowsViewModel.fetchShows()
-        self.listOfShowsTableView.reloadData()
+        listOfShowsViewModel.showsLoaded = { [weak self] (_, success) in
+            guard let self = self else { return }
+            if success {
+                self.listOfShowsTableView.reloadData()
+            }
+        }
     }
     
     @objc func refreshDataFromService() {
-        listOfShowsViewModel.shows?.removeAll()
+        listOfShowsViewModel.listOfShows?.removeAll()
         loadData()
     }
+}
+
+// MARK: - UI
+extension ListOfShowsViewController {
+    func setupUI() {
+        setupNavigationBar()
+        setupListOfShowsTableView()
+    }
+    
+    private func setupNavigationBar() {
+        navigationController?.navigationBar.topItem?.title = "TV Shows"
+        let refreshBarButtonItem =  UIBarButtonItem(title: "Refresh", style: .plain, target: self, action: #selector(refreshDataFromService))
+        self.navigationItem.rightBarButtonItem  = refreshBarButtonItem
+    }
+    
+    func setupListOfShowsTableView() {
+        view.addSubview(listOfShowsTableView)
+        listOfShowsTableView.fillToSuperview()
+        listOfShowsTableView.register(ShowTableViewCell.self, forCellReuseIdentifier: String(describing: ShowTableViewCell.self))
+        listOfShowsTableView.dataSource = self
+        listOfShowsTableView.delegate = self
+    }
+}
+
+// MARK: - UITableViewDataSource, UITableViewDelegate
+extension ListOfShowsViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listOfShowsViewModel.numberOfRows()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let showTableViewCell = tableView.dequeueReusableCell(
+            withIdentifier: String(describing: ShowTableViewCell.self),
+            for: indexPath
+        ) as? ShowTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        showTableViewCell.configure(serviceShow: listOfShowsViewModel.fetchShow(for: indexPath.row), realmShow: nil)
+
+        
+//        if listOfRealmShows == nil {
+//            showTableViewCell.configure(serviceShow: listOfServiceShows[indexPath.row], realmShow: nil)
+//        } else {
+//            showTableViewCell.configure(serviceShow: nil, realmShow: listOfRealmShows[indexPath.row])
+//        }
+        
+        return showTableViewCell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 130
+    }
+    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        let showImageURLString = listOfRealmShows == nil ? listOfServiceShows[indexPath.row].image?.medium : listOfRealmShows[indexPath.row].imageMedium
+//        let showName = listOfRealmShows == nil ? listOfServiceShows[indexPath.row].name : listOfRealmShows[indexPath.row].name
+//        let showLanguage = listOfRealmShows == nil ? listOfServiceShows[indexPath.row].language : listOfRealmShows[indexPath.row].language
+//        let showType = listOfRealmShows == nil ? listOfServiceShows[indexPath.row].type : listOfRealmShows[indexPath.row].type
+//        let showRating = listOfRealmShows == nil ? "\(listOfServiceShows[indexPath.row].rating?.average ?? 0)" : "\(listOfRealmShows[indexPath.row].rating)"
+//
+//        navigate(
+//            .init(
+//                pageType: .showDetailViewController(
+//                    showImageURLString: showImageURLString,
+//                    showName: showName,
+//                    showLanguage: showLanguage,
+//                    showType: showType,
+//                    showRating: showRating
+//                ),
+//                navigationStyle: .push(animated: true)
+//            )
+//        )
+//    }
 }
---
+++ Movies/ViewModels/ListOfShowsViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  ListOfShowsViewModel.swift
+//  Movies
+//
+//  Created by Nikos Angelidis on 12/5/23.
+//
+
+import Foundation
+
+final class ListOfShowsViewModel {
+    var showsLoaded: (([ServiceShow]?, Bool) -> Void)?
+    var listOfShows: [ServiceShow]?
+    
+    init() {
+        fetchShows()
+    }
+    
+    func fetchShows() {
+        ShowsAPI.shared.getShows { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let success):
+                self.listOfShows = success
+                self.handleResponse(response: success, success: true)
+            case .failure(_):
+                self.handleResponse(response: nil, success: false)
+            }
+        }
+    }
+    
+    func numberOfRows() -> Int {
+        return listOfShows?.count ?? 0
+    }
+    
+    func fetchShow(for index: Int) -> ServiceShow? {
+        return listOfShows?[index]
+    }
+    
+    private func handleResponse(response: [ServiceShow]?, success: Bool) {
+        if let showsLoaded = self.showsLoaded {
+            showsLoaded(response, success)
+        }
+    }
+}
